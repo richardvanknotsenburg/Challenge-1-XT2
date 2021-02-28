8d3bf69862b2615b40325b58bd751e6fb5fb5711
richardvanknotsenburg <61312996+richardvanknotsenburg@users.noreply.github.com>
2021-02-28 14:09:12 +0100

    maybe this works

--- _js/challenge1.js
+++ _js/challenge1.js
@@ -1,23 +1,46 @@
 var ctx = document.getElementById('myChart').getContext('2d');
+
+//Gradiënt kleur voor grafiek
+var gradientStroke = ctx.createLinearGradient(500, 200, 300, 200);
+gradientStroke.addColorStop(0, '#28ed00');
+
+gradientStroke.addColorStop(1, '#e30f00');
+
 var chart = new Chart(ctx, {
     // lijn grafiek
     type: 'line',
 
-    // data lijn grafiek
+    // data lijn grafiek + gradiënt toevoeging (puur om te oefenen)
     data: {
-        labels: ['January', 'March', 'April', 'May', 'June', 'July'],
+        labels: ['Lancering', '1e maand', '2e maand', '3e maand', '4e maand', '5e maand'],
         datasets: [{
-            label: 'Distance to go',
-            backgroundColor: 'rgba(43, 98, 186, 0.7)',
-            borderColor: 'rgba(25, 114, 255, 1)',
-            data: [0, 5, 2, 20, 30, 45]
+            label: 'Distance to go in Kilometers',
+            backgroundColor: 'rgba(255, 255, 255, 0.7)',
+            borderColor: gradientStroke,
+            pointBorderColor: gradientStroke,
+            pointBackgroundColor: gradientStroke,
+            pointHoverBackgroundColor: gradientStroke,
+            PointHoverBackgroundColor: gradientStroke,
+            pointBorderWidth: 10,
+            pointHoverRadius: 10,
+            pointHoverBorderWidth: 1,
+            PointRadius: 3,
+            fill: gradientStroke,
+            borderWidth: 4,
+            data: [400000, 346182, 301020, 239991, 187321, 143869]
         }]
     },
 
     // opties voor de style van de grafiek
    options: {
         legend: {
+            display: true,
+            labels: {
+                fontColor: 'rgba(255, 255, 255, 1)',
+                borderColor: 'rgba(25, 114, 255, 1)'
+            },
             position: "bottom"
+            
         },
 
         //geeft de properties weer voor de x- en de y-as
