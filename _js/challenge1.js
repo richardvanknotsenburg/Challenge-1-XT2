247e7b0c3b6a859309c72bee85efee58d6965b28
richardvanknotsenburg <61312996+richardvanknotsenburg@users.noreply.github.com>
2021-02-28 13:11:14 +0100

    heleboel zooi

--- _js/challenge1.js
+++ _js/challenge1.js
@@ -1,26 +1,83 @@
 var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
-    // The type of chart we want to create
+    // lijn grafiek
     type: 'line',
 
-    // The data for our dataset
+    // data lijn grafiek
     data: {
         labels: ['January', 'March', 'April', 'May', 'June', 'July'],
         datasets: [{
-            label: 'My First dataset',
+            label: 'Distance to go',
             backgroundColor: 'rgba(43, 98, 186, 0.7)',
             borderColor: 'rgba(25, 114, 255, 1)',
             data: [0, 5, 2, 20, 30, 45]
         }]
     },
 
-    // Configuration options go here
-    options: {
+    // opties voor de style van de grafiek
+   options: {
+        legend: {
+            position: "bottom"
+        },
+
+        //geeft de properties weer voor de x- en de y-as
+        scales: {
+            //y-as properties
+            yAxes: [{
+                ticks: {
+                    fontColor: "rgba(255,255,255,1)",
+                    fontStyle: "bold",
+                    beginAtZero: true,
+                    maxTicksLimit: 5,
+                    padding: 20
+                },
+                gridLines: {
+                    drawTicks: false,
+                    display: false
+                }
+}],
+            //x-as properties
+            xAxes: [{
+                gridLines: {
+                    zeroLineColor: "none"
+},
+                ticks: {
+                    padding: 20,
+                    fontColor: "rgba(255,255,255,1)",
+                    fontStyle: "bold"
+                }
+            }]
+        }
+    }
+});
+
+// een pie chart
+var ctx = document.getElementById('pieChart');
+var myPieChart = new Chart(ctx, {
+    type: 'pie',
+    data: data = {
+    datasets: [{
+        backgroundColor: 'rgba(25, 114, 255, 1)',
+        data: [10, 20, 30]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+        'Red', 
+        'Yellow',
+        'Blue'
+    ]
+},
+
+options: {
         legend: {
             display: true,
             labels: {
-                fontColor: 'rgba(25, 114, 255, 1)'
+                fontColor: 'rgba(255, 255, 255, 1)',
+                borderColor: 'rgba(25, 114, 255, 1)'
             }
         }
     }
+
+    
 });
